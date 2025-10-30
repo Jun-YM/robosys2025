d751b69c666064d23ac9222f77399fd32073445e
jun <0606jun4506@gmail.com>
2025-10-30 12:38:06 +0900

    テスト1

--- test.bash
+++ test.bash
@@ -1,4 +1,5 @@
-#!/bin/bash
+#!/bin/bash -xv
+# SPDX-FileCopyrightText: 2025 Junpei Yamamoto <s24C1133la@chibakoudai.jp>
 
 ng () {
         echo ${1}行目が違うヨ
@@ -6,9 +7,10 @@ ng () {
 }
 
 res=0
-a=山田
-[ "$a" = 上田 ] || ng "$LINENO" # LINENOは，この行の行番号の入る変数
-[ "$a" = 山田 ] || ng "$LINENO" # ngに第一引数として$LINENOを付与
 
+out=$(seq 5 | ./plus)
+[ "${out}" = 15.0 ] || ng "$LINENO"
+
+[ "${res}" = 0 ] && echo OK #通ったのが（人間に）分かるように表示
 exit $res
 
